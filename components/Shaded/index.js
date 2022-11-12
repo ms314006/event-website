2e93f479203922ad26cbeab324329fc97f79d2e6
GQSM <ms314006@hotmail.com>
2022-11-12 17:54:16 +0800

    ADD add props color, shadowColor, shadowTop to Saded

--- components/Shaded/index.js
+++ components/Shaded/index.js
@@ -1,17 +1,34 @@
 import styles from './Shaded.module.css'
 
-export default function Shaded(
-  { children, type, customStyles = {} }
-) {
+export default function Shaded({
+  children,
+  type,
+  color = '#D9EAF2',
+  shadowColor = '#86B6D3',
+  shadowTop = '0.5rem',
+  customStyles = {}
+}) {
   return (
     <div className={styles.shadedWrap} style={customStyles}>
       {
         type === 'button'
-          ? <button className={styles.shaded}>{ children }</button>
-          : <div className={styles.shaded}>{ children }</div>
+          ? (
+            <button className={styles.shaded} style={{ background: color }}>
+              { children }
+            </button>
+          )
+          : (
+            <div className={styles.shaded} style={{ background: color }}>
+              { children }
+            </div>
+          )
       }
-      
-      <div className={styles.shadedShadow}>{ children }</div>
+      <div
+        className={styles.shadedShadow}
+        style={{ background: shadowColor, top: shadowTop }}
+      >
+        { children }
+      </div>
       <div className={styles.hideContent}>{ children }</div>
     </div>
   )
