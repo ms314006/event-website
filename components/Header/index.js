e4afbf939f5e53bdfe4f9905ec9ad27f42a4f557
GQSM <ms314006@hotmail.com>
2022-11-13 21:27:15 +0800

    ADD Header responsive ui

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,26 +4,47 @@ import styles from './Header.module.css'
 
 export default function Header() {
   return (
-    <div className={styles.container}>
-      <Image
-        src="./assets/logo.png"
-        alt="Logo"
-        className={styles.logo}
-        width={150}
-        height={150}
-      />
-      <ul className={styles.menu}>
-        <li>關卡資訊</li>
-        <li>作品列表</li>
-        <li>攻略資源</li>
-        <li>求職專區</li>
-      </ul>
-      <div>
-        <Shaded type="button" customStyles={{ marginRight: 16 }}>
-          註冊報名
-        </Shaded>
-        <Shaded type="button">登入</Shaded>
+    <>
+      <div className={styles.container}>
+        <Image
+          src="./assets/logo.png"
+          alt="Logo"
+          className={styles.logo}
+          width={150}
+          height={150}
+        />
+        <ul className={styles.menu}>
+          <li>關卡資訊</li>
+          <li>作品列表</li>
+          <li>攻略資源</li>
+          <li>求職專區</li>
+        </ul>
+        <div>
+          <Shaded type="button" customStyles={{ marginRight: 16 }}>
+            註冊報名
+          </Shaded>
+          <Shaded type="button">登入</Shaded>
+        </div>
       </div>
-    </div>
+      <div className={styles.mobileContainer}>
+        <button className={styles.mobileHeaderButton}>
+          <Image
+            src="./assets/menu.png"
+            alt="Logo"
+            className={styles.logo}
+            width={16}
+            height={16}
+          />
+        </button>
+        <Image
+          src="./assets/logo.png"
+          alt="Logo"
+          className={styles.logo}
+          width={150}
+          height={150}
+        />
+        <button className={styles.mobileHeaderButton}>註冊報名</button>
+      </div>
+    </>
   )
 }
