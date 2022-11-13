58b30e1fe3e7cd7df01d209ee691a662fc32f528
GQSM <ms314006@hotmail.com>
2022-11-13 21:42:08 +0800

    UPDATE check position props set position type in skillIconWrap

--- components/SkillIconWrap/index.js
+++ components/SkillIconWrap/index.js
@@ -5,10 +5,11 @@ import styles from './skillIconWrap.module.css'
 export default function SkillIconWrap({ children, backgroundColor, border, position }) {
   return (
     <div
-      className={styles.SkillIconWrap}
+      className={styles.skillIconWrap}
       style={{
         background: backgroundColor,
         borderWidth: border ? '0.0625rem' : '0rem',
+        position: Boolean(position) ? 'absolute' : 'static',
         ...position,
       }}
     >
