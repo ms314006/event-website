643a36be5a595e1c9167f8901383aa0acbf75d63
GQSM <ms314006@hotmail.com>
2022-11-13 20:34:55 +0800

    build

---
+++ out/_next/static/rZA8arkm4btCY5qDlIppE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e49cf0693f63299d.css","static/chunks/pages/index-502de99c77e631c0.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
